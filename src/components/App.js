19516607018dc3c0ae58c7a5aac3698581733e07
Kacper Zieliński <zielinski.kac@gmail.com>
2019-02-13 14:28:26 +0100

    Dodano linki

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const Page1 = () => {
+	return (
+		<div>
+			<p>Page 1 </p>
+			<Link to="/2">Strona 2</Link>
+		</div>
+	);
+};
+
+const Page2 = () => {
+	return (
+		<div>
+			<p>Page 2 </p>
+			<Link to="/">Strona 1</Link>
+			<br />
+			<Link to="/2/3">Strona 3</Link>
+		</div>
+	);
+};
+
+const Page3 = () => {
+	return (
+		<div>
+			<p>Page 3 </p>
+			<Link to="/">Strona 1</Link>
+			<br />
+			<Link to="/2">Strona 2</Link>
+		</div>
+	);
+};
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" exact component={Page1} />
+					<Route path="/2" exact component={Page2} />
+					<Route path="/2/3" exact component={Page3} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
