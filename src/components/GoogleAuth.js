7dc7b494b17ad846bc30987b7175137cb2abcc73
Kacper Zieliński <zielinski.kac@gmail.com>
2019-02-14 14:54:24 +0100

    Refaktoryzacja do redux'a

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-
-export default class GoogleAuth extends Component {
-	state = { isSignedIn: null };
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
+class GoogleAuth extends Component {
 	componentDidMount() {
 		window.gapi.load('client:auth2', () => {
 			window.gapi.client
@@ -11,16 +11,16 @@ export default class GoogleAuth extends Component {
 				})
 				.then(() => {
 					this.auth = window.gapi.auth2.getAuthInstance();
-					this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+					this.onAuthChange(this.auth.isSignedIn.get());
 					this.auth.isSignedIn.listen(this.onAuthChange);
 				});
 		});
 	}
 
 	renderAuthButton() {
-		if (this.state.isSignedIn === null) {
+		if (this.props.isSignedIn === null) {
 			return null;
-		} else if (this.state.isSignedIn) {
+		} else if (this.props.isSignedIn) {
 			return (
 				<button className="ui red google button" onClick={this.wylogujClick}>
 					<i className="google icon" />
@@ -44,10 +44,26 @@ export default class GoogleAuth extends Component {
 	zalogujClick = () => {
 		this.auth.signIn();
 	};
-	onAuthChange = () => {
-		this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+	onAuthChange = (isSignedIn) => {
+		if (isSignedIn) {
+			this.props.signIn();
+		} else {
+			this.props.signOut();
+		}
 	};
 	render() {
 		return <div className="item">{this.renderAuthButton()}</div>;
 	}
 }
+
+const mapStateToProps = (state) => ({
+	isSignedIn: state.auth.isSignedIn
+});
+
+const mapDispatchToProps = {
+	signIn,
+	signOut
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoogleAuth);
