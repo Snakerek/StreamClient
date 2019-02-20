4d826774b1ef2470fc9e0fd8695ddc93e1284c0f
Kacper Zieliński <zielinski.kac@gmail.com>
2019-02-20 12:59:55 +0100

    Przekazywanie wartości do StreamEdit

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,5 +1,25 @@
-import React from 'react';
-
-export default function StreamEdit() {
-	return <div>StreamEdit</div>;
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+export class StreamEdit extends Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+	render() {
+		console.log(this.props);
+		if (!this.props.currentStream) {
+			return <div>Loading...</div>;
+		}
+		return <div>{this.props.currentStream.title}</div>;
+	}
 }
+
+const mapStateToProps = (state, ownProps) => ({
+	currentStream: state.streams[ownProps.match.params.id]
+});
+
+const mapDispatchToProps = {
+	fetchStream
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit);
