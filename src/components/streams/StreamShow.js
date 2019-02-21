26c07bcc4b53ba70d6bff687e13ec5459b94c10f
Kacper Zieliński <zielinski.kac@gmail.com>
2019-02-21 13:21:03 +0100

    Strona do wyświetlania pojedynczego streamu

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+export class StreamShow extends Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+	render() {
+		if (!this.props.currentStream) {
+			return <div>Loading...</div>;
+		}
 
-export default function StreamShow() {
-	return <div>StreamShow</div>;
+		const { title, description } = this.props.currentStream;
+		return (
+			<div>
+				<h1>{title}</h1>
+				<h5>{description}</h5>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = (state, ownProps) => ({
+	currentStream: state.streams[ownProps.match.params.id]
+});
+
+const mapDispatchToProps = {
+	fetchStream
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamShow);
